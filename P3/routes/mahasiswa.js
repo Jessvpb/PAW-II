55c374559300d1ea53083b6f51eac65537886eb0
Jessvpb <160694331+Jessvpb@users.noreply.github.com>
2024-09-26 11:29:39 +0700

    Commit-1
    Pertemuan 2-Pertemuan 5

---
+++ P3/routes/mahasiswa.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mahasiswaRouter = express.Router();
+
+mahasiswaRouter
+.route("/")
+.get((req,res) => { 
+    res.send("Get Data Mahasiswa");
+})
+.post((req, res) => {
+    res.send("Post Mahasiswa");
+});
+
+mahasiswaRouter
+.route("/:npm")
+.put((req, res) => {
+    res.send("Put Mahasiswa " + req.params.npm); // http://localhost:3000/mahasiswa/2226240090
+})
+.delete((req, res) => {
+    res.send("Delete Mahasiswa " + req.params.npm);
+});
+
+mahasiswaRouter.route("/kelas")
+.get((req, res) => {
+    const kelas = req.query.idkelas;
+    res.send("Kelasnya adalah " + kelas);
+});
+// CARA PANGGIL => http://localhost:3000/mahasiswa/kelas?idkelas=SI5B
+
+module.exports = mahasiswaRouter
